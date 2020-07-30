d21f91147301b3a7a5bf7a93c9970f3acb33f797
BO BO <861447653@qq.com>
2020-07-30 09:46:47 +0800

    练习

--- 01If语句/Program.cs
+++ 01If语句/Program.cs
@@ -9,6 +9,38 @@ namespace _01If语句
     {
         static void Main(string[] args)
         {
+            //if语句的写法：
+
+            //经典写法
+            if (true)
+            {
+                Console.WriteLine("这是IF语句的经典样式");
+            }
+
+            //直接写在一行
+            if (true)
+            {Console.WriteLine("这是缩在一起写");}
+
+            //缩写
+            if(true)
+                Console.WriteLine("缩写");
+
+            int intMine=0;
+
+            if (intMine>120)
+            {
+                Console.WriteLine("休息一下");
+            }
+
+            int intYes = 0;
+            if ((intYes%400==0)||(intYes%4==0 && intYes%100!=0))
+            {
+                Console.WriteLine("是闰年！");
+            }
+            else
+            {
+                Console.WriteLine("不是闰年");
+            }
         }
     }
 }
---
+++ 02if语句练习/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02if语句练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入您的考试成绩：");
+
+            int intGrade = Convert.ToInt16(Console.ReadLine());
+
+            if (intGrade<60)
+            {
+                Console.WriteLine("不及格，建议重修！");
+            }
+
+            else
+            {
+                if (intGrade>95)
+                {
+                    Console.WriteLine("牛逼");
+                }
+                Console.WriteLine("恭喜通过考试");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
